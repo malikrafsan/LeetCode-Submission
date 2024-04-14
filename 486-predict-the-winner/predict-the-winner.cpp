9dfff0a68648d99f71fabb492cc381353470e817
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-04-14 22:50:11 +0700

    Time: 12 ms (34.16%) | Memory: 13.8 MB (9.48%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,14 +1,33 @@
 class Solution {
+private:
+    map<string, pair<int, int>> mp;
+
 public:
+    string stringify(bool isPlayer1Turn, int lBound, int rBound) {
+        string key = "";
+        key += to_string(isPlayer1Turn);
+        key += to_string(lBound);
+        key += to_string(rBound);
+
+        return key;
+    }
+
     pair<int, int> best(vector<int>& nums, bool isPlayer1Turn, int lBound, int rBound) {
-        if (lBound >= rBound) return isPlayer1Turn ? make_pair(nums[rBound], 0) : make_pair(0, nums[rBound]);
+        auto key = stringify(isPlayer1Turn, lBound, rBound);
+        if (mp.count(key) != 0) return mp[key]; 
+
+        if (lBound >= rBound) {
+            auto res = isPlayer1Turn ? make_pair(nums[rBound], 0) : make_pair(0, nums[rBound]);
+            mp[key] = res;
+            return res;
+        }
         if (rBound - lBound == 1) {
             bool greaterLeft = nums[lBound] > nums[rBound];
             int maxPick = greaterLeft ? nums[lBound] : nums[rBound];
             int minPick = greaterLeft ? nums[rBound] : nums[lBound];
-            return isPlayer1Turn ? make_pair(maxPick, minPick) : make_pair(minPick, maxPick);
-            // int picked = nums[rBound] > nums[lBound] ? nums[rBound] : nums[lBound];
-            // int picked = std::max(nums[rBound], )
+            auto res = isPlayer1Turn ? make_pair(maxPick, minPick) : make_pair(minPick, maxPick);
+            mp[key] = res;
+            return res;
         }
         auto scoresWhenPickLeft = this->best(nums, !isPlayer1Turn, lBound+1, rBound);
         auto scoresWhenPickRight = this->best(nums, !isPlayer1Turn, lBound, rBound-1);
@@ -17,12 +36,16 @@ public:
             scoresWhenPickLeft.first += nums[lBound];
             scoresWhenPickRight.first += nums[rBound];
 
-            return scoresWhenPickLeft.first > scoresWhenPickRight.first ? scoresWhenPickLeft : scoresWhenPickRight;
+            auto res = scoresWhenPickLeft.first > scoresWhenPickRight.first ? scoresWhenPickLeft : scoresWhenPickRight;
+            mp[key] = res;
+            return res;
         } else {
             scoresWhenPickLeft.second += nums[lBound];
             scoresWhenPickRight.second += nums[rBound];
 
-            return scoresWhenPickLeft.second > scoresWhenPickRight.second ? scoresWhenPickLeft : scoresWhenPickRight;
+            auto res = scoresWhenPickLeft.second > scoresWhenPickRight.second ? scoresWhenPickLeft : scoresWhenPickRight;
+            mp[key] = res;
+            return res;
         }
     }
 
