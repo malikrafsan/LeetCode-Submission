9676a6f0f3de0dfc4461ce59839449342a8a7853
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-04-20 23:32:21 +0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (15.68%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,24 +1,37 @@
 class Solution {
 private:
-    map<string, pair<int, int>> mp;
+    // map<string, pair<int, int>> mp;
+    vector<vector<pair<int, int>>> dp1;
+    vector<vector<pair<int, int>>> dp2;
 
 public:
-    string stringify(bool isPlayer1Turn, int lBound, int rBound) {
-        string key = "";
-        key += to_string(isPlayer1Turn);
-        key += to_string(lBound);
-        key += to_string(rBound);
+    // string stringify(bool isPlayer1Turn, int lBound, int rBound) {
+    //     string key = "";
+    //     key += to_string(isPlayer1Turn);
+    //     key += to_string(lBound);
+    //     key += to_string(rBound);
 
-        return key;
+    //     return key;
+    // }
+
+    pair<int,int> getDP(bool isPlayer1Turn, int lBound, int rBound) {
+        return (isPlayer1Turn ? dp1 : dp2)[lBound][rBound];
+    }
+
+    void setDP(bool isPlayer1Turn, int lBound, int rBound, pair<int,int> elmt) {
+        (isPlayer1Turn ? dp1 : dp2)[lBound][rBound] = elmt;
     }
 
     pair<int, int> best(vector<int>& nums, bool isPlayer1Turn, int lBound, int rBound) {
-        auto key = stringify(isPlayer1Turn, lBound, rBound);
-        if (mp.count(key) != 0) return mp[key]; 
+        // auto key = stringify(isPlayer1Turn, lBound, rBound);
+        // if (mp.count(key) != 0) return mp[key]; 
+        auto dpElmt = (isPlayer1Turn ? dp1 : dp2)[lBound][rBound];
+        if (dpElmt != make_pair(-1,-1)) return dpElmt;
 
         if (lBound >= rBound) {
             auto res = isPlayer1Turn ? make_pair(nums[rBound], 0) : make_pair(0, nums[rBound]);
-            mp[key] = res;
+            // mp[key] = res;
+            setDP(isPlayer1Turn, lBound, rBound, res);
             return res;
         }
         if (rBound - lBound == 1) {
@@ -26,7 +39,8 @@ public:
             int maxPick = greaterLeft ? nums[lBound] : nums[rBound];
             int minPick = greaterLeft ? nums[rBound] : nums[lBound];
             auto res = isPlayer1Turn ? make_pair(maxPick, minPick) : make_pair(minPick, maxPick);
-            mp[key] = res;
+            // mp[key] = res;
+            setDP(isPlayer1Turn, lBound, rBound, res);
             return res;
         }
         auto scoresWhenPickLeft = this->best(nums, !isPlayer1Turn, lBound+1, rBound);
@@ -37,19 +51,25 @@ public:
             scoresWhenPickRight.first += nums[rBound];
 
             auto res = scoresWhenPickLeft.first > scoresWhenPickRight.first ? scoresWhenPickLeft : scoresWhenPickRight;
-            mp[key] = res;
+            // mp[key] = res;
+            setDP(isPlayer1Turn, lBound, rBound, res);
             return res;
         } else {
             scoresWhenPickLeft.second += nums[lBound];
             scoresWhenPickRight.second += nums[rBound];
 
             auto res = scoresWhenPickLeft.second > scoresWhenPickRight.second ? scoresWhenPickLeft : scoresWhenPickRight;
-            mp[key] = res;
+            // mp[key] = res;
+            setDP(isPlayer1Turn, lBound, rBound, res);
             return res;
         }
     }
 
     bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        this->dp1 = vector<vector<pair<int, int>>>(n, vector<pair<int, int>>(n, make_pair(-1, -1)));
+        this->dp2 = vector<vector<pair<int, int>>>(n, vector<pair<int, int>>(n, make_pair(-1, -1)));
+
         auto scores = this->best(nums, true, 0, nums.size()-1);
         return scores.first >= scores.second;   
     }
