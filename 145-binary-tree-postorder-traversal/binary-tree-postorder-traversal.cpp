4f7c9d0d5f5bb014a3d70cf0195c7fe11cfb3b90
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-04-20 19:38:47 +0700

    Time: 4 ms (32.29%) | Memory: 9.3 MB (99.90%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -20,8 +20,43 @@ public:
     }
 
     vector<int> postorderTraversal(TreeNode* root) {
+        if (root==nullptr) return {};
+
         vector<int> res;
-        dfs(res, root);
+        vector<TreeNode*> stack;
+        stack.push_back(root);
+        while (1) {
+            if (stack.empty()) break;
+
+            auto cur = stack.back();
+
+            if (cur->left == nullptr && cur->right == nullptr) {
+                res.push_back(cur->val);
+                stack.pop_back();
+            }
+
+            if (cur->right) {
+                stack.push_back(cur->right);
+                cur->right = nullptr;
+            }
+            if (cur->left) {
+                stack.push_back(cur->left);
+                cur->left = nullptr;
+            }
+
+
+
+            // if (stack.empty() && root == nullptr) break;
+
+            // auto cur = stack.back();
+            // stack.pop_back();
+
+            // if (cur == nullptr) continue;
+
+            // stack.push_back(cur->left);
+            // stack.push_back(cur->right);
+        }
+
         return res;
     }
 };
