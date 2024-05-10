832817f27b716e69a9b519695eb8f46a21959f5e
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-05-11 01:43:29 +0700

    Time: 31 ms (77.94%) | Memory: 16.5 MB (30.47%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -7,12 +7,26 @@ class Solution:
         
         # return self.climbStairs(n-1) + self.climbStairs(n-2)
 
-        arr = [0 for _ in range(max(n+1, 3))]
-        arr[1] = 1
-        arr[2] = 2
+        # arr = [0 for _ in range(max(n+1, 3))]
+        # arr[1] = 1
+        # arr[2] = 2
 
-        for i in range(3, n+1):
-            arr[i] = arr[i-1] + arr[i-2]
+        # for i in range(3, n+1):
+        #     arr[i] = arr[i-1] + arr[i-2]
         
-        return arr[n]
+        # return arr[n]
+
+        var1 = 1
+        if n == 1: return var1
+
+        var2 = 2
+        if n == 2: return var2
+
+        res = None
+        for i in range(n-2):
+            res = var1 + var2
+            var1 = var2
+            var2 = res
+        
+        return res
 
