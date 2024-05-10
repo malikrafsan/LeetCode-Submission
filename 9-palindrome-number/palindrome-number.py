ffe6cb072cf12b12534492ff607a85582aa721f3
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-05-11 01:35:30 +0700

    Time: 64 ms (17.01%) | Memory: 16.7 MB (16.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,9 +1,18 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:        
-        sx = str(x)
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
 
-        for i in range(len(sx)//2):
-            if sx[i] != sx[len(sx)-i-1]:
+        arr = []
+        while True:
+            arr.append(x % 10)
+            x = x//10
+
+            if x == 0:
+                break      
+
+        for i in range(len(arr) // 2):
+            if arr[i] != arr[len(arr) - i - 1]:
                 return False
-        
-        return True
+
+        return True        
