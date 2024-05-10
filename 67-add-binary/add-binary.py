b755caa8322c194d1bc9105431b8b80005c947dc
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-05-10 19:44:41 +0700

    Time: 46 ms (8.55%) | Memory: 16.6 MB (69.86%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,89 +1,47 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        ai = int(a, 2)
-        bi = int(b, 2)
+        # ai = int(a, 2)
+        # bi = int(b, 2)
 
-        si = ai + bi
+        # si = ai + bi
 
-        return bin(si)[2:]
+        # return bin(si)[2:]
 
-        # n1 = len(a)
-        # n2 = len(b)
+        n1 = len(a)
+        n2 = len(b)
 
-        # idx1 = n1-1
-        # idx2 = n2-1
+        idx1 = n1-1
+        idx2 = n2-1
 
-        # continuing = False
-        # res = ""
-        # while True:
-        #     if idx1 < 0 or idx2 < 0:
-        #         break
+        continuing = False
+        res = ""
+        while True:
+            if idx1 < 0 and idx2 < 0 and (not continuing):
+                break
             
-        #     c1 = a[idx1]
-        #     c2 = b[idx2]
+            ctr = 0
+            if continuing:
+                ctr += 1
+                continuing = False
 
-        #     ctr = 1 if continuing else 0
-        #     continuing = False
-
-        #     ctr += 1 if c1 == "1" else 0
-        #     ctr += 1 if c2 == "1" else 0
-
-        #     if ctr % 2 == 0:
-        #         res = "0" + res
-        #     else:
-        #         res = "1" + res
+            if idx1 >= 0:
+                c1 = a[idx1]
+                ctr += 1 if c1 == "1" else 0
+                idx1 -= 1
             
-        #     if ctr >= 2:
-        #         continuing = True
+            if idx2 >= 0:
+                c2 = b[idx2]
+                ctr += 1 if c2 == "1" else 0
+                idx2 -= 1
+
+            if ctr % 2 == 0:
+                res += "0"
+            else:
+                res += "1"
             
-        #     idx1 -= 1
-        #     idx2 -= 1
+            if ctr >= 2:
+                continuing = True
 
-        # while True:
-        #     if idx1 < 0:
-        #         break
-
-        #     c1 = a[idx1]
-
-        #     ctr = 1 if continuing else 0
-        #     continuing = False
-
-        #     ctr += 1 if c1 == "1" else 0
-
-        #     if ctr % 2 == 0:
-        #         res = "0" + res
-        #     else:
-        #         res = "1" + res
-
-        #     if ctr >= 2:
-        #         continuing = True
-            
-        #     idx1 -= 1
-
-        # while True:
-        #     if idx2 < 0:
-        #         break
-
-        #     c2 = b[idx2]
-
-        #     ctr = 1 if continuing else 0
-        #     continuing = False
-
-        #     ctr += 1 if c2 == "1" else 0
-
-        #     if ctr % 2 == 0:
-        #         res = "0" + res
-        #     else:
-        #         res = "1" + res
-            
-        #     if ctr >= 2:
-        #         continuing = True
-            
-        #     idx2 -= 1
-
-        # if continuing:
-        #     res = "1" + res
-
-        # return res
+        return res[::-1]
 
 
