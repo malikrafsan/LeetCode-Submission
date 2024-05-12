9e1b1599a6df61c65b39cf4156dac6f1a340ed2e
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-05-12 20:52:31 +0700

    Time: 90 ms (60.13%) | Memory: 18.4 MB (66.91%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = {c: [] for c in range(numCourses)}
+        for p in prerequisites:
+            graph[p[0]].append(p[1])
+        
+        output = []
+        visit = [False for _ in range(numCourses)]
+        cycle = set()
+
+        def dfs(src: int):
+            if src in cycle:
+                return False
+            
+            if visit[src]:
+                return True
+            
+            cycle.add(src)
+            for nb in graph[src]:
+                if not dfs(nb):
+                    return False
+            
+            cycle.remove(src)
+
+            visit[src] = True
+            output.append(src)
+            return True
+        
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+        
+        return output
+    
