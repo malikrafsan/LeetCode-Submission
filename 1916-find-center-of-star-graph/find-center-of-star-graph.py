2dcfaa8bc6df0001ce558078020cef6392ab27e6
Malik Akbar Hashemi Rafsanjani <77711133+malikrafsan@users.noreply.github.com>
2024-05-12 19:39:34 +0700

    Time: 655 ms (69.59%) | Memory: 52.1 MB (91.25%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        mp = {}
+        
+        edge1 = edges[0]
+        edge2 = edges[1]
+
+        arr = [edge1[0], edge1[1], edge2[0], edge2[1]]
+        for elmt in arr:
+            if elmt not in mp:
+                mp[elmt] = 1
+            else:
+                mp[elmt] += 1
+        
+        print(arr)
+        print(mp)
+        for key in mp:
+            if mp[key] == 2:
+                return key
+
+        return -1
